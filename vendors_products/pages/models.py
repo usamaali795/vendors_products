824937c7cfae1f9beb7e35960262bfc0673a2f31
Usama Ali <ua197@student.aru.ac.uk>
2024-06-19 12:04:14 +0100

    added models, views, and and urls

--- vendors_products/pages/models.py
+++ vendors_products/pages/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
 
-# Create your models here.
+
+fileSystem = FileSystemStorage(location='pages/static')
+
+
+
+class Vendor(models.Model):
+    companyName = models.CharField(max_length=255)
+    established = models.DateField()
+    employeesCount = models.IntegerField()
+    internalProfessionalService = models.BooleanField()
+    lastDemo = models.DateField()
+    lastReview = models.DateField()
+    companyDisplayPicture = models.ImageField(storage=fileSystem,upload_to='company_display_pics/')
+    companyBackgroundPicture = models.ImageField(storage=fileSystem,upload_to='company_background_pics/')
+    company_url = models.URLField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.companyName
+
+class Product(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='products')
+    productName = models.CharField(max_length=255)
+    type = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(storage=fileSystem, upload_to='product_images/')
+    business_area = models.CharField(max_length=255)
+    modules = models.CharField(max_length=255)
+    financialServicesClientTypes = models.CharField(max_length=255)
+    cloud = models.BooleanField()
+
+    def __str__(self):
+        return self.productName
--- vendors_products/pages/views.py
+++ vendors_products/pages/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Vendor, Product
 
-# Create your views here.
+def vendor_list(request):
+    vendors = Vendor.objects.all()
+    print(vendors[0].companyDisplayPicture)
+    return render(request, 'vendor/vendors_list.html', {'vendors': vendors})
+
+def vendor_detail(request, vendor_id):
+    vendor = get_object_or_404(Vendor, id=vendor_id)
+    return render(request, 'vendor/vendor_details.html', {'vendor': vendor})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'product/product_detail.html', {'product': product})
